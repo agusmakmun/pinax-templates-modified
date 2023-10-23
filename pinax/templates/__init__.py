21ed6804ab96a0453722c11f4b9e0bfb616ce589
Agus Makmun <agus@practical-devsecops.com>
2023-10-23 10:54:43 +0700

    fix: DistributionNotFound

--- pinax/templates/__init__.py
+++ pinax/templates/__init__.py
@@ -2,4 +2,4 @@ import pkg_resources
 
 
 default_app_config = "pinax.templates.apps.AppConfig"
-__version__ = pkg_resources.get_distribution("pinax-templates").version
+__version__ = pkg_resources.get_distribution("pinax-templates-modified").version
